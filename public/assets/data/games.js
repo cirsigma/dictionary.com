29fce9ab73347e6b7c295f5686c6dff64140e46e
cirsigma <cirweber@hotmail.com>
2024-11-25 17:07:09 -0500

    games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -4,6 +4,13 @@ var games = [
     title: 'Add custom game',
     image: 'assets/images/add.png'
   },
+{
+    id: 'idlebreakout',
+    title: 'Idle Breakout',
+    url: 'https://www.gamenora.com/game/rooftop-snipers/',
+    image: 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
+  },
+  
   {
     id: 'idlebreakout',
     title: 'Idle Breakout',
